abc4532e311cb46fa4a0d49e0a1be94daa122a39
0kim <ceo.youngkim@gmail.com>
2021-05-27 21:13:19 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+const app = express();
+const port = 5000;
