c5dda16f5860705f4a649d8d503feb647bd27a2d
0kim <ceo.youngkim@gmail.com>
2021-06-03 21:35:00 +0900

    add description

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer(props) {
             <p className="footer_title">0Kim</p>
                 <a href="" className="footer_icon"><i className="fab fa-github" onClick={()=> window.open("about:blank").location.href="https://github.com/youngkim90"}></i></a>
                 <a href="" className="footer_icon"><i className="fab fa-blogger-b" onClick={()=> window.open("about:blank").location.href="https://0kim.tistory.com"}></i></a>
-            <p>&#169; 2020 copyright all right reserved</p>
+            <p>&#169; 2021 copyright all right reserved</p>
         </footer>
     );
 }
